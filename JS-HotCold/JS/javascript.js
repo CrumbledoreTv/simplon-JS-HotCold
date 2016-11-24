b7d1bf3d9cec8f6590743ae2d32e4fe3bff1b0b6
Saii62 <ellart.simon@gmail.com>
2016-11-24 16:55:26 +0100

    HotnCold-v4

--- JS-HotCold/JS/javascript.js
+++ JS-HotCold/JS/javascript.js
@@ -2,64 +2,65 @@
 console.log("******* DEBUT SCRIPT *******");
 
 /* Nombre choisi par le PC */
-var nbPc = Math.floor(100*Math.random());
-console.log("Nombre PC :"+ nbPc);
+var nbPc = Math.floor(100 * Math.random());
+console.log("Nombre PC :" + nbPc);
 
-var essai= 0;
+var essai = 0;
 /********************* FUNCTION ********************************/
-function game(){
-  var indication = document.getElementById('indic');
+function game() {
+    var indication = document.getElementById('indic');
 
-  var temp = document.getElementById("temp");
-  var tempR = document.getElementById("tempR");
+    var temp = document.getElementById("temp");
+    var tempR = document.getElementById("tempR");
 
-  var win = document.getElementById("work");
-  var coup = document.getElementById("win");
+    var win = document.getElementById("work");
+    var coup = document.getElementById("win");
 
-  essai++;
+    essai++;
 
-  /* Nombre choisi par l'User */
-  var nombre = document.getElementById("nombreUser").value;
-  console.log("Nombre User :" +nombre);
+    /* Nombre choisi par l'User */
+    var nombre = document.getElementById("nombreUser").value;
+    console.log("Nombre User :" + nombre);
 
-/*********** GAME *************/
-if(essai == 5){
-  alert("Indice : le nombre est compris entre "+(nbPc-Math.floor(Math.random()*12)-2)+" et "+(nbPc+Math.floor(Math.random()*12+2)));
-}else if(essai<=9){
-    if(nombre == nbPc){
-      win.style.visibility = "visible";
-      temp.style.backgroundColor = "red";
-      temp.style.transition ="2s";
-      temp.style.height = "250px";
-      tempR.style.backgroundColor = "red";
+    /*********** GAME *************/
+    if (essai == 5) {
+        alert("Indice : le nombre est compris entre " + (nbPc - Math.floor(Math.random() * 12) - 2) + " et " + (nbPc + Math.floor(Math.random() * 12 + 2)));
+    } else if (essai <= 9) {
+        if (nombre == nbPc) {
+            win.style.visibility = "visible";
+            temp.style.backgroundColor = "red";
+            temp.style.transition = "2s";
+            temp.style.height = "250px";
+            tempR.style.backgroundColor = "red";
 
-      coup.innerHTML = "Vous avez gagné en "+essai+" coups";
-      console.log("******* FIN SCRIPT *******");
-    }else if(Math.abs(nbPc - nombre) <= 10){
-      temp.style.backgroundColor = "red";
-      temp.style.transition ="2s";
-      temp.style.height = "230px";
+            coup.innerHTML = "Vous avez gagné en " + essai + " coups";
+            console.log("******* FIN SCRIPT *******");
+        } else if (Math.abs(nbPc - nombre) <= 10) {
+            temp.style.backgroundColor = "red";
+            temp.style.transition = "2s";
+            temp.style.height = "230px";
 
 
-      tempR.style.backgroundColor = "red";
+            tempR.style.backgroundColor = "red";
+            tempR.style.transition = "2s";
 
-    }else if(Math.abs(nbPc - nombre) <= 20){
-      temp.style.backgroundColor = "orange";
-      temp.style.transition ="2s";
-      temp.style.height = "150px";
+        } else if (Math.abs(nbPc - nombre) <= 20) {
+            temp.style.backgroundColor = "orange";
+            temp.style.transition = "2s";
+            temp.style.height = "150px";
 
-      tempR.style.backgroundColor = "orange";
-      tempR.style.transition ="2s";
+            tempR.style.backgroundColor = "orange";
+            tempR.style.transition = "2s";
 
-    }else{
-      temp.style.backgroundColor = "blue";
-      temp.style.transition ="2s";
-      temp.style.height = "50px";
+        } else {
+            temp.style.backgroundColor = "blue";
+            temp.style.transition = "2s";
+            temp.style.height = "50px";
 
-      tempR.style.backgroundColor = "blue";
-      tempR.style.transition ="2s";
+            tempR.style.backgroundColor = "blue";
+            tempR.style.transition = "2s";
+        }
+    } else {
+        alert("Vous avez perdu ! Le chiffre était : " + nbPc);
     }
-  }else{
-    alert("Vous avez perdu ! Le chiffre était : "+nbPc);
-  }
 }
